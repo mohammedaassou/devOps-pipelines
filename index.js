c55dfdc88c7aed4d13819f23daf547eff6defad8
Mohamed Aassou <mohamedaassou12@gmail.com>
2025-12-11 17:20:09 +0100

    feat:(CI/CD): add CI pipeline

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Hello devOps")
+
+console.log("I am learning CI/CD using Github actions")
+
+
---
+++ test.js
@@ -0,0 +1,10 @@
+
+
+console.log("starting test")
+
+
+setTimeout(() => {
+    console.log("waiting 3 seconds...")
+}, 3000);
+
+console.log("test complete")
